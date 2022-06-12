deec1dacb3d1b44826fd3d3250956915a2d7e9d9
glebite <glebite@gmail.com>
2022-06-12 15:20:39 -0400

    Next revision fun!

--- src/badstring.py
+++ src/badstring.py
@@ -8,6 +8,7 @@ class BadString:
     """
     def __init__(self):
         self._internal = ""
+        self._impairments = list()
 
     @property
     def internal(self):
@@ -20,3 +21,17 @@ class BadString:
     @internal.deleter
     def internal(self):
         del self._internal
+
+    @property
+    def impairments(self):
+        return self_impairments
+
+    @impairments.setter
+    def impairments(self, impairment_function):
+        self._impairments.append(impairment_function)
+
+    @impairments.deleter
+    def impairments(self):
+        del self._impairments
+
+    
